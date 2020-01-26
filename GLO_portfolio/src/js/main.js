60bc52c5f5f88fafd4c088b334d1061c775448e7
IgorVL <mail@igorvl.ru>
2020-01-26 16:11:11 +0300

    мобильная и десктоп добавлен слайдер GLO portfolio

---
+++ GLO_portfolio/src/js/main.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+    // el: '.swiper-pagination',
+    el: '.projects__tabs',
+    type: 'bullets',
+    bulletClass: 'projects__bullet',
+    bulletActiveClass: 'projects__bullet-active',
+    clickable: true,
+  },
+});
