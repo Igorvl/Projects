0a3d1ec6036a3982860dc6f972afe3281a98f69d
IgorVL <mail@igorvl.ru>
2020-02-09 15:30:34 +0300

    Witcher мобильная версия, слайдер, верхнее меню

--- Witcher/src/js/main.js
+++ Witcher/src/js/main.js
@@ -1,9 +1,23 @@
 var mySwiper = new Swiper('.swiper-container', {
   speed: 400,
-  spaceBetween: 30,
-  slidesPerView: 2,
+  slidesPerView: 1,
   loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
   },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is <= 670px
+    670: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  }
 });
+
+const menuButton = document.querySelector('.mobile-menu')
+const menu = document.querySelector('.header')
+
+menuButton.addEventListener('click', (param)=>{
+  menu.classList.toggle('header__active')
+})
