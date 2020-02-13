c1183f63e87189693a9e5312496291cd50aad966
IgorVL <mail@igorvl.ru>
2020-02-14 00:10:55 +0300

    request

---
+++ components/Main/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import cls from './Profile.module.css'
+
+export default () => {
+  return (
+    <main className={cls.main}>
+      <div className={cls.main__img}></div>
+      <div className={cls.main__person}>
+
+      </div>
+      <div className={cls.main__post}></div>
+      <div className={cls.main__fraise}></div>
+    </main>
+  )
+}
---
+++ components/header_comps/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+export default (props) => {
+  return (
+    <div className="head">
+      <ul>
+        <li><a href="https://ya.ru">test</a></li>
+        <li><a href="https://ya.ru">test</a></li>
+        <li><a href="https://ya.ru">test</a></li>
+        <li><a href="https://ya.ru">test</a></li>
+        <li><a href="https://ya.ru">test</a></li>
+      </ul>
+    </div>
+  )
+}
