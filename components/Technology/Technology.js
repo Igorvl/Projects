9964711180f62015f9ced5b2b043144479f82bf2
IgorVL <mail@igorvl.ru>
2020-02-13 17:10:30 +0300

    Samura 13+

---
+++ components/Technology/Technology.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Technology = () => {
+  return (
+    <div className="Technology">
+      <h1>Samura</h1>
+      <div className="diff">Sama diffa</div>
+    </div>
+  )
+}
+
+export default Technology
