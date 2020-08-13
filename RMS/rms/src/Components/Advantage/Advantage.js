fbf79792daccb33f775bc3beef100311c0a8577e
IgorVL <mail@igorvl.ru>
2020-08-13 03:38:38 +0300

    Install fonts

--- RMS/rms/src/Components/Advantage/Advantage.js
+++ RMS/rms/src/Components/Advantage/Advantage.js
@@ -1,15 +1,19 @@
 import React from "react";
-import s from "../css/Advantage.module.css";
+import s from "../../css/Advantage.module.css";
 
-export default () => {
+export default (props) => {
 	return (
 		<div className={s.global_wrapper}>
-			<h2>
+			<h2 className={s.adv_H2}>
 				Ваши лучшие результаты в бизнесе
 			</h2>
-			<h3>
+			<h3 className={s.adv_H3}>
 				Доверьте создание сайта RMS, и вы заметите разницу:
 			</h3>
+			
+			<span className={s.adv_remark}>
+				Отчет на основе опроса клиентов RMS
+			</span>
 		
 		</div>
 	)
